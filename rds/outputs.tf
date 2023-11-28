fad6b14c325a3963fb5ac3fcd0f2a158e22dee13
Christoph Herb <ch.herb@gmx.de>
2023-11-28 20:00:37 +0100

    make passwords in rds sensitive

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -9,11 +9,13 @@ output "endpoint" {
 }
 
 output "master_username" {
-  value = aws_rds_cluster.book_db.master_username
+  value     = aws_rds_cluster.book_db.master_username
+  sensitive = true
 }
 
 output "master_password" {
-  value = random_password.password.result
+  value     = random_password.password.result
+  sensitive = true
 }
 
 output "database_name" {
